18bfb15ff0743b8be4e19b100624e04a6e99ba46
albert73 <albert73@ecbiz172.inmotionhosting.com>
2017-07-26 13:56:30 -0400

    first working configuration of a blog

---
+++ site/accounts/albert.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: albert
+firstname: Albert
+lastname: ""
+email: sonic_down@yahoo.com
+password: >
+  $2a$10$89kNpUs95F5tgmMkABG57u4rvi/cS/9RY/8.fd1q34yNuGiTj9gVq
+language: en
+role: editor
---
+++ site/templates/home.php
@@ -0,0 +1,46 @@
+<?php snippet('header') ?>
+
+<main>
+
+  <?php 
+  $perpage=$pages->visible()->perpage()->int();
+  $list=$pages->children()->visible()->flip()->paginate($perpage); 
+  $pagination = $list->pagination();
+  ?>
+
+  <?php foreach($list as $article): ?>
+    <article>
+    <h1><?php echo $article->title()->html() ?></h1>
+    <h1><?php echo $article->date('Y-m-d') ?></h1>
+    <p><?php echo $article->text()->excerpt(300) ?></p>
+    <a href="<?php echo $article->url() ?>">Read more…</a>
+    </article>
+  <?php endforeach ?>
+
+
+<nav>
+  <ul>
+
+    <?php if($pagination->hasPrevPage()): ?>
+    <li><a href="<?php echo $pagination->prevPageURL() ?>">&larr;</a></li>
+    <?php else: ?>
+    <li><span>&larr;</span></li>
+    <?php endif ?>
+
+    <?php foreach($pagination->range(10) as $r): ?>
+    <li><a<?php if($pagination->page() == $r) echo ' class="active"' ?> href="<?php echo $pagination->pageURL($r) ?>"><?php echo $r ?></a></li>
+    <?php endforeach ?>
+
+    <?php if($pagination->hasNextPage()): ?>
+    <li class="last"><a href="<?php echo $pagination->nextPageURL() ?>">&rarr;</a></li>
+    <?php else: ?>
+    <li class="last"><span>&rarr;</span></li>
+    <?php endif ?>
+
+  </ul>
+</nav>
+
+
+</main>
+
+<?php snippet('footer') ?>
