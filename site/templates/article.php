0ec91f19aad6bdc7480808b351c2ee4f66b5ae8b
albertandfei <albert73@ecbiz172.inmotionhosting.com>
2017-08-14 21:57:12 -0400

    updated article.php to look same as the main page (for now, until the main page is changed again) so previewing posts is more accurate

--- site/templates/article.php
+++ site/templates/article.php
@@ -1,36 +1,47 @@
 <?php snippet('header') ?>
 
-<div class=container>
-<div class="col-md-3 col-xs-1"></div>
+<div class=row>
+  <div class="col-md-2 col-xs-1"></div>
+  <div class="col-md-8 col-xs-10">
+  <!-- begin main site content -->
+
+  <main>
+    <!-- setting up variables -->
+    <?php 
+      $article=$page;
+    ?>
+
+    <!-- print out the blog articles -->
+      <article>
+      <p class=article-title><?php echo $article->title()->html() ?></p>
+      <?php 
+        $image=$article->coverimage()->toFile();
+        if ($image):
+      ?>
+        <img src="<?= $image->url() ?>" alt="" class=coverimage>
+      <?php endif ?>
+      <div class=article-date>Posted on <?php echo $article->date('F j, Y') ?> by <?php echo $article->author() ?></div>
+      <?php if ($image): ?>
+        <hr class=coverimage>
+      <?php endif ?>
 
 
-<div class="col-md-6 col-xs-10">
-
-  <!--<main class="main" role="main">-->
-    
-    <!--<article class="article single wrap">-->
-
-      <header class="article-header">
-        <h1><?= $page->title()->html() ?></h1>
-        <div class="intro text">
-          <?= $page->date('F jS, Y') ?>
-        </div>
-        <hr />
-      </header>
-      
-      <?php snippet('coverimage', $page) ?>
-      
       <div class="text">
-        <?= $page->text()->kirbytext() ?>
+        <?= $article->text()->kirbytext() ?>
       </div>
-      
-    <!--</article>-->
-    
-    <?php snippet('prevnext', ['flip' => true]) ?>
-    
-  <!--</main>-->
 
+      <!-- todo: make sure last article doesnt have an hr -->
+      <hr class=article>
+
+
+      </article>
+
+
+  </main>
+
+  <!-- end main site content -->
+  </div>
+  <div class="col-md-2 col-xs-1"></div>
 </div>
-<div class="col-md-3 col-xs-1"></div>
 
 <?php snippet('footer') ?>
